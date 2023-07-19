9e17635d16604f4b357c14a63a7bc1672758030d
DotWith <dotwith@tutanota.com>
2023-07-19 03:04:26 -0400

    Added TypePath

--- bevy_b3d/src/lib.rs
+++ bevy_b3d/src/lib.rs
@@ -19,7 +19,7 @@ impl Plugin for B3DPlugin {
 }
 
 /// Representation of a loaded b3d file.
-#[derive(Debug, TypeUuid)]
+#[derive(Debug, TypeUuid, TypePath)]
 #[uuid = "bea01a2e-984c-4e7d-ace8-64a580e882cc"]
 pub struct B3D {
     pub scene: Handle<Scene>,
@@ -29,7 +29,7 @@ pub struct B3D {
 }
 
 /// A b3d node with all of its child nodes, its [`B3DMesh`] and [`Transform`]
-#[derive(Debug, TypeUuid)]
+#[derive(Debug, TypeUuid, TypePath)]
 #[uuid = "c0375473-fdfa-4f47-99d7-60ab2f5c6b88"]
 pub struct B3DNode {
     pub children: Vec<B3DNode>,
@@ -38,7 +38,7 @@ pub struct B3DNode {
 }
 
 /// A b3d mesh, which may contists of a [`Mesh`] and an optional [`StandardMaterial`].
-#[derive(Debug, TypeUuid)]
+#[derive(Debug, TypeUuid, TypePath)]
 #[uuid = "26c8d9d7-2197-4ce9-a250-1b9a8b775689"]
 pub struct B3DMesh {
     pub mesh: Handle<Mesh>,
